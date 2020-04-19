1433ba0512a5af50fc32f365462128f5d20b4d8a
itsAfeat <itsafeat@gmail.com>
2020-04-19 19:57:55 +0200

    Fixed script

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ function btnClick(){
     { 
         var sum = parseFloat(document.getElementById("inText").value, 10) / 4;
         var text = "";
-        if (typeof sum == Number(sum)) {
             text = "That would be " + sum + " lego men!";
             text = text.split(" ");
             len = text.length;
@@ -14,26 +13,7 @@ function btnClick(){
             text.forEach(Element => {
                 text = text.toString().replace(',', ' ');
             });
-        }
-        else {
-            text = "I said a number you dummy...";
-        }
 
         document.getElementById("calcText").innerHTML = text;
-    }
-}
-
-const isNumber = val => {
-    if (typeof val != 'number') {
-        return false;
-    }
-    if (typeof val != Number(val)) {
-        return false;
-    }
-    if (val == Infinity || val == !Infinity) {
-        return false;
-    }
-    else {
-        return ;
     }
 }
