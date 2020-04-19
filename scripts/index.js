fb3cab978a6663725e844c936245ee6be21e7e54
itsAfeat <itsafeat@gmail.com>
2020-04-19 20:00:41 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,15 @@ function btnClick(){
     { 
         var sum = parseFloat(document.getElementById("inText").value, 10) / 4;
         var text = "";
-            text = "That would be " + sum + " lego men!";
-            text = text.split(" ");
-            len = text.length;
-            result = [];
+        text = "That would be " + sum + " lego men!";
+        text = text.split(" ");
+        len = text.length;
+        result = [];
         
-            text[3] = '<u>' + sum + '</u>';
+        text[3] = '<u>' + sum + '</u>';
 
-            text.forEach(Element => {
-                text = text.toString().replace(',', ' ');
+        text.forEach(Element => {
+            text = text.toString().replace(',', ' ');
             });
 
         document.getElementById("calcText").innerHTML = text;
